6d25f82cde80c67454f1c9211266e9634feacd49
Isis Lázaro <isislazaro@gmail.com>
2019-11-27 14:14:36 -0600

    initialize a map with mapbox

---
+++ src/mapping.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiaW1sciIsImEiOiJjanljbWowbDgwanZlM2xzOGZqcDFxbHVuIn0.niCENs8TKhS-7PSMjHFE3A";
+
+const map = new mapboxgl.Map({
+  container: "map", // container id
+  style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+  center: [-99, 19], // starting position [lng, lat]
+  zoom: 5 // starting zoom
+});
