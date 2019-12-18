efd444ea037daeb8f89f59a5a95ee64338a41262
Isis Lázaro <isislazaro@gmail.com>
2019-12-18 14:25:43 -0600

    stop scroll overflow during modals

--- src/modal.js
+++ src/modal.js
@@ -1,12 +1,17 @@
 document.querySelectorAll(".button-modal").forEach(btn => {
   btn.addEventListener("click", () => {
     const modal = document.querySelector(btn.getAttribute("data-target"));
+    const html = document.querySelector("html");
+
     modal.classList.add("is-active");
+    html.classList.add("is-clipped");
     modal.querySelector(".modal-close").addEventListener("click", () => {
       modal.classList.remove("is-active");
+      html.classList.remove("is-clipped");
     });
     modal.querySelector(".modal-background").addEventListener("click", () => {
       modal.classList.remove("is-active");
+      html.classList.remove("is-clipped");
     });
   });
 });
